cb41b40677e6a6837196d740cf6370a9392f01c0
William <122514675+Wilcun@users.noreply.github.com>
2025-06-09 14:25:24 +0700

    Delete docs directory

--- docs/mataPelajaran/matapelajaran.js
+++
@@ -1,9 +0,0 @@
-// // Placeholder interaksi tombol
-// document.querySelectorAll('.detail-btn').forEach(btn => {
-//   btn.addEventListener('click', () => {
-//     alert("Fitur Lihat Detail akan segera tersedia.");
-//   });
-// });
-
-
-
