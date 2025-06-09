030385a6c15afc1d04d0cb62159a62be966c471f
William <122514675+Wilcun@users.noreply.github.com>
2025-06-09 15:21:51 +0700

    Add files via upload

---
+++ docs/mataPelajaran/matapelajaran.js
@@ -0,0 +1,9 @@
+// // Placeholder interaksi tombol
+// document.querySelectorAll('.detail-btn').forEach(btn => {
+//   btn.addEventListener('click', () => {
+//     alert("Fitur Lihat Detail akan segera tersedia.");
+//   });
+// });
+
+
+
